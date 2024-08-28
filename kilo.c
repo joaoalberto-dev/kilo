89c45dbb69650f2e0342ec5d14d8d1cd3482ab61
João Alberto <hey@joaoalberto.dev>
2024-08-28 00:35:58 -0300

    chore: adding timeout to read char

--- kilo.c
+++ kilo.c
@@ -24,6 +24,8 @@ void enableRawMode(void)
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -34,8 +36,12 @@ int main(void)
 
     char c;
 
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
+        char c = '\0';
+
+        read(STDIN_FILENO, &c, 1);
+
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
@@ -44,6 +50,9 @@ int main(void)
         {
             printf("%d ('%c')\r\n", c, c);
         }
+
+        if (c == 'q')
+            break;
     }
 
     return 0;
